92a139ee7526a66ac742aec4db6965cb73c25871
Phil White <philgewhite@gmail.com>
2026-08-15 12:37:08 +0100

    Bump cache to v13

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // deletes every cache that isn't the current name, so raising the version is
 // what actually evicts a stale copy from a device that has been running the
 // app for a while.
-const CACHE_NAME = 'dashboard-v12';
+const CACHE_NAME = 'dashboard-v13';
 const CORE_ASSETS = [
 './',
 './index.html',
